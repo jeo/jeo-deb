299ee995c5a9152292126af7f46717326026fe05
Justin Deoliveira <jdeolive@gmail.com>
2015-02-28 20:07:24 +0000

    Updated package to the latest version and added jeo-gdal package.

--- build.sh
+++ build.sh
@@ -3,19 +3,25 @@
 set -x
 set -e
 
-ver=0.3
+ver=0.5
 zip=jeo-$ver-cli.zip
-url=http://ares.boundlessgeo.com/jeo/release/$ver/$zip
+gdal_tgz=gdaljni-1.11.1-linux64.tgz
+
+url=https://github.com/jeo/jeo-cli/releases/download/$ver/$zip
+gdal_url=https://github.com/jeo/jeo-cli/releases/download/$ver/$gdal_tgz
 
 if [ -e $zip ]; then
   rm $zip
+fi
+if [ -e $gdal_tgz ]; then
+  rm $gdal_tgz
 fi
 
 if [ -e tmp ]; then
   rm -rf tmp 
 fi
 
-rm -rf *.txt bin lib 
+rm -rf LICENSE.md bin lib ext jni
 
 if [ "$1" == "clean" ]; then
   exit
@@ -26,6 +32,10 @@ unzip -d tmp $zip
 pushd tmp/jeo-$ver
 mv * ../../
 popd
+
+wget $gdal_url
+mkdir jni
+tar xzvf $gdal_tgz -C jni
 
 debuild clean
 debuild -uc -us -b -i
